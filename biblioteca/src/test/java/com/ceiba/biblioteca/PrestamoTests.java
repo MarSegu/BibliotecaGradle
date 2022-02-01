8bdfcf93d212fe520fc50d6bd92a5354ed8ef421
MarSegu <mariosegura0904@gmail.com>
2022-02-01 11:11:35 -0500

    Devolución cambio test

--- biblioteca/src/test/java/com/ceiba/biblioteca/PrestamoTests.java
+++ biblioteca/src/test/java/com/ceiba/biblioteca/PrestamoTests.java
@@ -196,8 +196,7 @@ class PrestamoTests {
                 .content(objectMapper.writeValueAsString(new SolicitudPrestarLibroTest("EQWQW8545", "1111111111", USUARIO_DESCONOCIDO))))
                 .andDo(print())
                 .andExpect(status().is4xxClientError())
-                .andExpect(jsonPath("$.mensaje", is("Tipo de usuario no permitido en la biblioteca")))
-                .andExpect(jsonPath("$.tipoMensaje").exists());
+                .andExpect(jsonPath("$.mensaje", is("Tipo de usuario no permitido en la biblioteca")));
     }
 
 
